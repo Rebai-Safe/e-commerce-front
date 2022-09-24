c8d57109fb5e215f3aba651be13d0e6f24b5f3f6
Rebai-Safe <saferebai@hotmail.com>
2022-09-24 14:13:04 +0100

    add image upload

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
+import { FileHandle } from '../model/file-handle.model';
 import { Product } from '../model/product.model';
 import { ProductService } from '../services/product.service';
 
@@ -10,20 +12,21 @@ import { ProductService } from '../services/product.service';
 })
 export class AddProductComponent implements OnInit {
 
-  product: Product={
+product: Product={
     productName: "",
     productDescription: "",
     productDiscountedPrice: 0,
-    productActualPrice: 0
+    productActualPrice: 0,
+    productImages: []
     
   }
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
   }
   
   addProduct(productForm: NgForm){
-   this.productService.addProduct(this.product).subscribe(
+   this.productService.addProduct(this.prepareFormData(this.product)).subscribe(
     (response) => {
       productForm.reset();
     },
@@ -32,4 +35,37 @@ export class AddProductComponent implements OnInit {
     }
    )
   }
+ 
+  prepareFormData(product: Product): FormData{
+    const formData= new  FormData ();
+    formData.append(
+      'product',
+      new Blob([JSON.stringify(product)], {type:'application/json'})
+    );
+
+    for(var i=0; i<product.productImages.length;i++){
+      formData.append(
+        'imageFile',
+        product.productImages[i].file,
+        product.productImages[i].file.name
+      )
+    }
+
+    return formData;
+  }
+
+  onFileSelected(event: any) {
+    if(event.target.files){
+      const file = event.target.files[0];
+      const fileHandle: FileHandle = {
+        file: file,
+        url: this.sanitizer.bypassSecurityTrustUrl(
+          window.URL.createObjectURL(file))
+      }
+
+      this.product.productImages.push(fileHandle);
+    }
+     
+    
+    }
 }
